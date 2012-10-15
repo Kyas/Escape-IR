a4b14a090804d1e19ee434969232457489ab6613
LE ROUX Thomas <git@november-eleven.fr>
2012-10-15 20:40:20 +0200

    Initial Commit.

---
+++ src/fr/escape/Launcher.java
@@ -0,0 +1,28 @@
+/*****************************************************************************
+ * 
+ * Copyright 2012 LE ROUX Thomas <thomas@november-eleven.fr>
+ * 
+ * This file is part of Escape-IR.
+ * 
+ * Escape-IR is free software: you can redistribute it and/or modify
+ * it under the terms of the zlib license. See the COPYING file.
+ * 
+ *****************************************************************************/
+
+package fr.escape;
+
+/**
+ * Default Launcher/Main for Escape-IR
+ */
+public final class Launcher {
+
+	/**
+	 * Default Entry Point
+	 * 
+	 * @param args Options, if any.
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+	
+}
---
+++ src/fr/escape/Version.java
@@ -0,0 +1,22 @@
+/*****************************************************************************
+ * 
+ * Copyright 2012 LE ROUX Thomas <thomas@november-eleven.fr>
+ * 
+ * This file is part of Escape-IR.
+ * 
+ * Escape-IR is free software: you can redistribute it and/or modify
+ * it under the terms of the zlib license. See the COPYING file.
+ * 
+ *****************************************************************************/
+
+package fr.escape;
+
+/**
+ * The version of Escape-IR
+ */
+public final class Version {
+
+	/** The current version of Escape-IR in the major.minor format **/
+	public final static String VERSION = "0.5";
+
+}
