fcfaa8caf0b2284c564a7273a9f578d3cac10d08
LE ROUX Thomas <git@november-eleven.fr>
2012-10-17 00:24:48 +0200

    Improve Intern Clock for FPS

--- src/fr/escape/app/Graphics.java
+++ src/fr/escape/app/Graphics.java
@@ -11,21 +11,29 @@
 
 package fr.escape.app;
 
+import fr.escape.Escape;
+
 public final class Graphics {
+	
+	private final static int FPS_DEFAULT = 60;
+	private final static int MINIMUM_WAKEUP_TIME = 4;
+	private final static int MAXIMUM_WAKEUP_TIME = 32;
 	
 	private final int width;
 	private final int heigt;
 	
-	private long lastRender;
+	volatile private long lastRender;
 	private int rawFps;
-	private int smothFps;
+	volatile private int smoothFps;
+	volatile private int wakeUp;
 	
 	public Graphics(int width, int height) {
 		this.width = width;
 		this.heigt = height;
 		this.lastRender = System.currentTimeMillis();
 		this.rawFps = 0;
-		this.smothFps = 0;
+		this.smoothFps = 0;
+		this.wakeUp = 25;
 	}
 	
 	/** 
@@ -50,10 +58,18 @@ public final class Graphics {
 	}
 	
 	/** 
-	 * @return The average number of frames per second
+	 * @return The average number of frames per second.
 	 */
 	public int getFramesPerSecond() {
-		return smothFps;
+		return smoothFps;
+	}
+	
+	public int getRequestedFramesPerSecond() {
+		return FPS_DEFAULT;
+	}
+	
+	public int getNextWakeUp() {
+		return wakeUp;
 	}
 	
 	public void render() {
@@ -62,11 +78,41 @@ public final class Graphics {
 		rawFps++;
 		
 		if((lastRender / 1000) < (currentRender / 1000)) {
-			smothFps = (int) ((0.1 * smothFps) + (0.9 * rawFps));
-			rawFps = 0;
+			updateFramesPerSecond();
+			updateWait();
+			Escape.activity.log("Graphics - FPS", String.valueOf(getFramesPerSecond()));
 		}
 		
 		lastRender = currentRender;
 	}
 	
+	/**
+	 * Update the average number of frames per second.
+	 */
+	private void updateFramesPerSecond() {
+		smoothFps = (int) ((0.1 * smoothFps) + (0.9 * rawFps));
+		rawFps = 0;
+	}
+	
+	private void updateWait() {
+		
+		if(((getFramesPerSecond() < getRequestedFramesPerSecond()) &&
+				(wakeUp > MINIMUM_WAKEUP_TIME)) || 
+				((getFramesPerSecond() > getRequestedFramesPerSecond()) &&
+				(wakeUp < MAXIMUM_WAKEUP_TIME))) {
+			
+			int factor = getRequestedFramesPerSecond() - getFramesPerSecond();
+			wakeUp = 1000 / (getRequestedFramesPerSecond() + factor);
+			
+			if(wakeUp < MINIMUM_WAKEUP_TIME) {
+				wakeUp = MINIMUM_WAKEUP_TIME;
+			}
+			
+			if(wakeUp > MAXIMUM_WAKEUP_TIME) {
+				wakeUp = MAXIMUM_WAKEUP_TIME;
+			}
+			
+		}
+		
+	}
 }
