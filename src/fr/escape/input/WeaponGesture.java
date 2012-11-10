914a11de536322efd6148896c95b4d2cbdb3eb07
tquantin <quantin.thomas@gmail.com>
2012-11-10 01:32:51 +0100

    Correction Drift + Direction deplacement shot

--- src/fr/escape/input/WeaponGesture.java
+++ src/fr/escape/input/WeaponGesture.java
@@ -3,6 +3,7 @@ package fr.escape.input;
 import java.util.List;
 
 import fr.escape.app.Input;
+import fr.escape.game.entity.CoordinateConverter;
 
 // TODO Comment
 public final class WeaponGesture implements Gesture {
@@ -14,10 +15,13 @@ public final class WeaponGesture implements Gesture {
 		float cd = (end.getY() - start.getY()) / (end.getX() - start.getX());
 		System.out.println(cd);
 		
+		float distanceX = CoordinateConverter.toMeterX(end.getX() - start.getX()) / 10;
+		float distanceY = CoordinateConverter.toMeterX(end.getY() - start.getY()) / 10;
+				
   		//TODO calcul velocity
   		velocity[0] = 1000.f;
-  		velocity[1] = 0.f;
-  		velocity[2] = -1.f;
+  		velocity[1] = distanceX;
+  		velocity[2] = distanceY;
   		
   		return true;
 	}
